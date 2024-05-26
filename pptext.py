87a5f57da48194ab4c67f4cf219f7ab1f3a10d9a
David Britt <dkbritt7174@gmail.com>
2024-05-26 11:34:34 -0400

    Condensing python file

--- pptext.py
+++ pptext.py
@@ -11,24 +11,11 @@ import pyprimesieve
 
 __copyright__ = "Copyright (C) 09-7-2023 David Kevin Britt"
 __license__ = "GPL 3.1"
-__version__ = "1.1.1"
+__version__ = "2.0"
 __maintainer__ = "David Kevin Britt"
 __email__ = "dkbritt7174@gmail.com"
 __status__ = "Production"
 
-
-#    This program is free software and can be edited and modified.
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 def main():
     """
