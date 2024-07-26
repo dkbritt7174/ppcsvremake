7eaff61a5977de101528516bd6b3af624f79ff92
David Britt <dkbritt7174@gmail.com>
2024-07-26 03:40:15 -0400

    Updated from replit

--- pptext.py
+++ pptext.py
@@ -1,7 +1,6 @@
 # !/usr/bin/python
 
-"""
-    Prime Number Finder
+"""    Prime Number Finder list in file primelist.txt
 """
 
 import sys
@@ -9,40 +8,33 @@ import time
 import pyprimesieve
 
 
-__copyright__ = "Copyright (C) 09-7-2023 David Kevin Britt"
-__license__ = "GPL 3.1"
-__version__ = "2.0"
-__maintainer__ = "David Kevin Britt"
-__email__ = "dkbritt7174@gmail.com"
-__status__ = "Production"
+
+print('\nUsage: python ppcsv.py (N) where N is the upper limit')
+print('\nFor Example python ppcsv.py 10000\nPrimes found in primelist.txt\n','\n')
 
 
 def main():
+    """     Usage: python ppcsv.py (N) where N is the upper limit.
+            For Example python ppcsv.py 10000   Primes found in primelist.csv
     """
-    Usage: python ppcsv.py (N) where N is the upper limit.
-    For Example python ppcsv.py 10000   Primes found in primelist.csv
-    """
-
-
-try:
-    LIMIT_MAX = sys.argv[1]
-except IndexError:
-    print(main.__doc__)
 
 LIMIT_MAX = sys.argv[1]
-csvprime_list = [pyprimesieve.primes(int(LIMIT_MAX))]
+txtPrimelist = [pyprimesieve.primes(int(LIMIT_MAX))]
 start_time = time.perf_counter()
 
 # If you wish to print to screen, uncomment next line.
 # print(csvprime_list)
 
 with open('primelist.txt', 'w', encoding='utf8') as fp:
-    for item in csvprime_list:
+    for item in txtPrimelist:
         fp.write(str(item))
+
+print(len(item), ' primes found.\n')
 
 run_time = (time.perf_counter() - start_time) * 1000
 if run_time < 1000:
     print("\n", run_time, " milliseconds")
+
 else:
     print("\n", run_time / 1000, " seconds")
 
